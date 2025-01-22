d298cf9fe84cd6f810ea66b56836e228766a3cf3
24c02 <163450896+24c02@users.noreply.github.com>
2025-01-21 20:06:04 -0500

    changes :3

--- shirt.rb
+++ shirt.rb
@@ -52,11 +52,37 @@ end
 QR_SIZE = 110
 SPACING = 5
 ITEMS_PER_ROW = 3
-ROWS_PER_PAGE = 4
+ROWS_PER_PAGE = 5
 ITEMS_PER_PAGE = ITEMS_PER_ROW * ROWS_PER_PAGE
 
-PAGE_HEIGHT = 800
+PAGE_HEIGHT = 820
 PAGE_WIDTH = 612
+
+ARTS = {
+  0 => {
+    file: './art_2.png',
+    next_row_offset: 30,
+    rows_til_offset: 0,
+    opts: {
+      at: [PAGE_WIDTH * 0.5 + 10 + 10, PAGE_HEIGHT - 40],
+      width: PAGE_WIDTH * 0.33
+    }
+  },
+  1 => {
+    file: './art_3.png',
+    opts: {
+      at: [-20, PAGE_HEIGHT - QR_SIZE - 170],
+      width: QR_SIZE * 2.2
+    }
+  },
+  3 => {
+    file: './art_1.png',
+    opts: {
+                  at: [PAGE_WIDTH * 0.6 - 30, PAGE_HEIGHT - (QR_SIZE * 3) - 185 - 20 - 15 - 10],
+                  width: QR_SIZE * 2.1
+    }
+  }
+}
 
 def shiperize(person)
   png_filez = []
@@ -75,6 +101,12 @@ def shiperize(person)
     end
   end.values
 
+  # # testing:
+  # # for one ship:
+  # ships = [ships.first]
+  # # for more ships:
+  # ships.concat(ships)
+
   ships.reject! { |ship|
     ship['hours'] == 0 ||
     ship['deploy_url'].blank? ||
@@ -91,7 +123,7 @@ def shiperize(person)
     end
   end
 
-  def generate_qr(ship, x, y, pdf)
+  def generate_qr(ship, x, y, pdf, size=QR_SIZE, font_size=10)
       qr = RQRCode::QRCode.new(ship['deploy_url'])
       png = qr.as_png(
         bit_depth: 1,
@@ -104,32 +136,32 @@ def shiperize(person)
         resize_gte_to: false,
       )
 
-      pdf.image StringIO.new(png.to_blob), at: [x, y], width: QR_SIZE
+      pdf.image StringIO.new(png.to_blob), at: [x, y], width: size
 
       pdf.text_box ship['title'],
-                   at: [x, y - QR_SIZE + 2],
-                   width: QR_SIZE,
-                   height: 10,
+                   at: [x, y - size + 2],
+                   width: size,
+                   height: font_size,
                    align: :center,
-                   size: 10,
+                   size: font_size,
                    overflow: :shrink_to_fit
 
-      x_center = x + (QR_SIZE / 2)
-      stats_y = y - QR_SIZE - 15
+      x_center = x + (size / 2)
+      stats_y = y - size - 15
 
       content = ""
       content_width = 0
-      icon_size = 10
+      icon_size = font_size
 
       if ship['hours']
         display_hours = number_to_rounded(ship['hours'], precision: 1, strip_insignificant_zeros: true)
-        hours_text = "#{display_hours}h"
-        content_width += pdf.width_of(hours_text, size: 10)
+        hours_text = "#{display_hours}h /"
+        content_width += pdf.width_of(hours_text, size: font_size)
       end
 
       if ship['doubloons']
         doubloon_value = ship['doubloons'].to_f.round(0).to_s
-        content_width += pdf.width_of(doubloon_value, size: 10) + icon_size + 4
+        content_width += pdf.width_of(doubloon_value, size: font_size) + icon_size + 4
         content_width += 5 if ship['hours']
       end
 
@@ -139,24 +171,27 @@ def shiperize(person)
       if ship['hours']
         pdf.text_box hours_text,
                      at: [current_x, stats_y],
-                     size: 10
-        current_x += pdf.width_of(hours_text, size: 10) + 5
+                     size: font_size
+        current_x += pdf.width_of(hours_text, size: font_size) + 5
       end
 
       if ship['doubloons']
         pdf.text_box doubloon_value,
                      at: [current_x, stats_y],
-                     size: 10
+                     size: font_size
 
         pdf.image "./doubloon.png",
-                  at: [current_x + pdf.width_of(doubloon_value, size: 10) + 2, stats_y + 2],
+                  at: [current_x + pdf.width_of(doubloon_value, size: font_size) + 2, stats_y + 2],
                   width: icon_size
       end
-
-      File.delete(qr_path)
   end
 
   ships.each_slice(ITEMS_PER_PAGE).with_index do |page_ships, page_index|
+    pending_offset = rto = nil
+    row_shifts = [:left, :left, :right, :right].cycle
+    row_shifts.next
+
+    offset = 0
     puts "page #{page_index + 1}"
     pdf = Prawn::Document.new(page_size: [PAGE_WIDTH, PAGE_HEIGHT])
 
@@ -176,45 +211,49 @@ def shiperize(person)
     end
     pdf.fill_color '000000'
 
+
     pdf.text_box "@#{handle} - #{'project'.pluralize(ships.length)}",
-                  at: [PAGE_WIDTH * 0.05, PAGE_HEIGHT - 90],
-                  height: 40,
-                  width: PAGE_WIDTH * 0.4,
-                  align: :left,
-                  size: 30,
-                  overflow: :shrink_to_fit
+                 at: [PAGE_WIDTH * 0.05, PAGE_HEIGHT - 90],
+                 height: 30,
+                 width: ((QR_SIZE + SPACING) * 3) - (PAGE_WIDTH * 0.15),
+                 # ^ number 100% pulled out of ass ^
+                 align: :left,
+                 size: 30,
+                 min_font_size: 8,
+                 overflow: :shrink_to_fit
 
-    page_ships.each_slice(ITEMS_PER_ROW).with_index do |row_ships, row_index|
-      if row_index == 0 || row_index == 3
-        generate_qr_row row_ships,
-                        0,
-                        PAGE_HEIGHT - QR_SIZE - (row_index * (QR_SIZE + SPACING + 20)),
-                        :left,
-                        pdf
+
+    case page_ships.length
+    when 1
+      pdf.image ARTS[0][:file], **ARTS[0][:opts]
+      generate_qr(page_ships.first, 55, PAGE_HEIGHT-113, pdf, QR_SIZE*1.8, 21)
+    when 2
+      pdf.image ARTS[0][:file], **ARTS[0][:opts]
+      generate_qr(page_ships[0], 5,        PAGE_HEIGHT-113, pdf, QR_SIZE*1.3, 13)
+      generate_qr(page_ships[1], 5+143+15, PAGE_HEIGHT-113, pdf, QR_SIZE*1.3, 13)
+    else
+      page_ships.each_slice(ITEMS_PER_ROW).with_index do |row_ships, row_index|
+        row_shift = row_shifts.next
+          generate_qr_row row_ships,
+                          row_shift == :right ? QR_SIZE * 2.1 : 0,
+                          PAGE_HEIGHT - QR_SIZE - (row_index * (QR_SIZE + SPACING + 20)) + offset,
+                          row_shift,
+                          pdf
+        if (art = ARTS[row_index])
+          pdf.image art[:file], **art[:opts]
+          pending_offset = art[:next_row_offset]
+          rto = art[:rows_til_offset]
+        end
+        if pending_offset
+          if rto == 0
+            offset -= pending_offset
+            pending_offset = rto = nil
+          else
+            rto -= 1
+          end
+        end
       end
-      if row_index == 1 || row_index == 2
-        generate_qr_row row_ships,
-                        QR_SIZE * 2.1,
-                        PAGE_HEIGHT - QR_SIZE - (row_index * (QR_SIZE + SPACING + 20)),
-                        :right,
-                        pdf
       end
-    end
-
-    pdf.image "./art_2.png",
-              at: [PAGE_WIDTH * 0.5 + 10, PAGE_HEIGHT - 40],
-              width: PAGE_WIDTH * 0.33
-    if page_ships.length > 6
-      pdf.image "./art_3.png",
-                at: [-20, PAGE_HEIGHT - QR_SIZE - 170],
-                width: QR_SIZE * 2.2
-    end
-
-    if page_ships.length > 9
-      pdf.image "./art_1.png",
-                at: [PAGE_WIDTH * 0.6, PAGE_HEIGHT - (QR_SIZE * 3) - 185],
-                width: QR_SIZE * 2.1
-    end
 
     pdf_filename = "./output/#{person.nice_full_name.downcase.gsub(' ', '_')}_shirt_#{page_index + 1}.pdf"
     png_filename = pdf_filename.sub('.pdf', '.png')
